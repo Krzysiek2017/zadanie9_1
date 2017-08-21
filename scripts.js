ab2087bb1edc87f97055d896e9a366bbdf6f849a
Logan <musashimiyamoto@interia.eu>
2017-08-21 20:24:22 +0200

    changes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
-function getTriangleArea (a, h) {
-  var triangleArea = a*h/2;
-  if ((a <= 0) || (h <= 0)) {
-    console.log("Nieprawidłowe dane");
-    return; }
-    return triangleArea;
-  }
+function getTriangleArea(a, h) {
+	var triangleArea = a * h / 2;
+	if ((a <= 0) || (h <= 0)) {
+		console.log("Nieprawidłowe dane");
+		return;
+	}
+	return triangleArea;
+}
 
-console.log(getTriangleArea(10, 6) );
+console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15);
-var triangle2Area = getTriangleArea(20, 30);
+
+console.log(getTriangleArea(10, 15));
+
+var triangle2Area = getTriangleArea(-20, -30);
+
+console.log(getTriangleArea(-20, -30));
+
 var triangle3Area = getTriangleArea(8, 16);
+
+console.log(getTriangleArea(8, 16));
